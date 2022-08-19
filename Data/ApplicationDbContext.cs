948ef9c4797806fb7a2e5ce0c41edeeb2de9fa93
zynpakclgl <55785717+zynpakclgl@users.noreply.github.com>
2022-08-19 14:41:00 +0300

    Proje dosyası ekle.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace olcutBelirleme.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Unit> Units { get; set; }
+    }
+}
