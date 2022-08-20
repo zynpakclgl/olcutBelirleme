cd2ef7f9c880042d0cf815c8beae5c931cbba2fa
Merve Yılmaz <56924476+merviwe@users.noreply.github.com>
2022-08-20 23:59:48 +0300

    eklemeler yapıldıDüzenleme

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using olcutBelirleme.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace olcutBelirleme.Controllers
@@ -20,15 +22,20 @@ namespace olcutBelirleme.Controllers
 
         public IActionResult Index()
         {
+          
+           
             return View();
         }
 
         public IActionResult Privacy()
         {
+          
+            
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,7 +12,5 @@ namespace olcutBelirleme.Data
         {
 
         }
-
-        public virtual DbSet<Unit> Units { get; set; }
     }
 }
