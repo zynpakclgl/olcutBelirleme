126fca5b4eb68f23ac113d23fc4a8160347462f2
zynpakclgl <55785717+zynpakclgl@users.noreply.github.com>
2022-09-01 00:01:53 +0300

    Navbar düzenleme

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using olcutBelirleme.Models;
+using System.Diagnostics;
 
 namespace olcutBelirleme.Controllers
 {
     public class AboutController : Controller
     {
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public AboutController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+
+
             return View();
+        }
+
+        public IActionResult About()
+        {
+
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
